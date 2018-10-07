94afb3dcd988e2d52bb9568dc71365fffca1c7ed
olga <vpitac@gmail.com>
2018-10-07 11:48:54 -0500

    version 1.0
    Signed-off-by: olga <vpitac@gmail.com>

--- Int_Bolitas.py
+++ Int_Bolitas.py
@@ -1,9 +1,25 @@
 from graphics import *
+
 ventana=GraphWin("CIRCULO",500,500)
 Circulo=Circle(Point(40,40),40)
 Circulo.draw(ventana)
 Circulo.setFill("blue")
 
+while 1==1:
+    for i in range (0,84):
+        Circulo.move(5,5)
+        time.sleep(0.05)
 
+    for j in range (0,84):
+        Circulo.move(0,-5)
+        time.sleep(0.05)
 
-time.sleep(60)
+    for k in range (0,84):
+        Circulo.move(-5,5)
+        time.sleep(0.05)
+
+    for i in range (0,84):
+        Circulo.move(0,-5)
+        time.sleep(0.05)
+
+time.sleep(10)
